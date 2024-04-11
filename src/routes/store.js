7830872fef0a74cf1b8f18833edabbfa3f6b7905
chayapon <chayapon.c@gmail.com>
2024-04-11 08:49:20 +0700

    Add sessionStorage for taskList

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,10 +1,24 @@
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
 let taskList = writable([]);
 let idCounter = writable(0);
 let numCheckedTasks = writable(0);
 let checkedTaskId = writable([]);
 let isChecked = writable(false);
+
+if (browser) {
+	const initialTasks = sessionStorage.getItem("task")
+		? JSON.parse(sessionStorage.getItem("task"))
+		: [];
+	taskList.set(initialTasks);
+}
+
+taskList.subscribe((updatedTasks) => {
+	if (browser) {
+		sessionStorage.setItem("task", JSON.stringify(updatedTasks));
+	}
+});
 
 const clearTaskList = () => {
 	taskList.set([]);
